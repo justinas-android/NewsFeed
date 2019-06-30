d8af2fc84c0342d4d8f96351735e910a25ab0911
unknown <justinastvarijonas@gmail.com>
2019-06-30 22:07:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.newsfeed;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.newsfeed.api.ApiClient;
+import com.example.newsfeed.api.ApiInterface;
+import com.example.newsfeed.models.Article;
+import com.example.newsfeed.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "6ba3d055c4c34457858e2eff7d737882";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        LoadJson();
+
+    }
+
+    private void initListener(){
+        adapter.setOnItemClickListener(new Adapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Article article = articles.get(position);
+                String url = article.getUrl();
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void LoadJson(){
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country, API_KEY);
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful() && response.body().getArticle() != null){
+                    if(!articles.isEmpty()){
+                        articles.clear();
+                    }
+                    articles = response.body().getArticle();
+                    adapter = new Adapter(articles, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    initListener();
+                }else{
+                    Toast.makeText(MainActivity.this, "No Results", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/newsfeed/models/News.java
@@ -0,0 +1,47 @@
+package com.example.newsfeed.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class News {
+
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("totalResults")
+    @Expose
+    private String totalResults;
+
+    @SerializedName("articles")
+    @Expose
+    List<Article> article;
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Article> getArticle() {
+        return article;
+    }
+
+    public void setArticle(List<Article> article) {
+        this.article = article;
+    }
+}
